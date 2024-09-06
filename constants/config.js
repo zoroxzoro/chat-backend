9713bc5530f5930b7207ebc4f6416db340ded7e8
kaif alam <126477482+zoroxzoro@users.noreply.github.com>
2024-09-06 14:12:44 +0530

    Update config.js

--- constants/config.js
+++ constants/config.js
@@ -4,6 +4,7 @@ const corsOptions = {
     "http://localhost:4173",
     process.env.CLIENT_URL,
     "chat-frontend-delta-five.vercel.app",
+    "chat-frontend-peach.vercel.app"
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
