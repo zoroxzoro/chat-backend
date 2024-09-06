7526f6d2d792cd29b575245fb640c1ba0a57573e
zoroxzoro <Kaifalam9580@gmail.com>
2024-09-06 13:38:19 +0530

    new commit 2

--- constants/config.js
+++ constants/config.js
@@ -3,6 +3,7 @@ const corsOptions = {
     "http://localhost:5173",
     "http://localhost:4173",
     "http://localhost:5000/",
+    process.env.CLIENT_URL,
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
--- index.js
+++ index.js
@@ -22,6 +22,7 @@ import adminRoutes from "./routes/admin.routes.js";
 import ChatRoutes from "./routes/chat.routes.js";
 import dotenv from "dotenv";
 import { log } from "console";
+import { corsOptions } from "./constants/config.js";
 dotenv.config();
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -33,29 +34,16 @@ cloudinary.config({
 const app = express();
 const server = createServer(app);
 const io = new Server(server, {
-  cors: {
-    origin: [
-      "http://localhost:5173",
-      "https://chat-backend-h3rn.onrender.com/",
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  },
+  cors: corsOptions,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
 });
 
 app.set("io", io);
 
 // Middleware
 app.use(express.json());
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "https://chat-backend-h3rn.onrender.com/",
-    ],
-    credentials: true,
-  })
-);
+app.use(cors(corsOptions));
 app.use(cookieParser());
 
 // Socket middleware
