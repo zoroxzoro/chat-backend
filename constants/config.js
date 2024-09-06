eaf8cf0775bdaa33635c432a75a9fc401467ec1e
zoroxzoro <Kaifalam9580@gmail.com>
2024-09-06 13:57:33 +0530

    cors correction

--- constants/config.js
+++ constants/config.js
@@ -3,6 +3,7 @@ const corsOptions = {
     "http://localhost:5173",
     "http://localhost:4173",
     process.env.CLIENT_URL,
+    "chat-frontend-delta-five.vercel.app",
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
